522db0ed25eca836333ae6755fa0911d31f17854
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-05 20:35:23 +0900

    テスト追加

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,7 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe "Events", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET events' do
+    context 'イベントが存在するとき' do
+      let!(:event1) { create(:event) }
+      let!(:event2) { create(:event) }
+
+      before do
+        get '/events'
+      end
+
+      it '200 OK が返ってくる' do
+        expect(response).to be_successful
+        expect(response.status).to eq 200
+      end
+
+      it '登録したイベントが全て返ってくる' do
+        json = JSON.parse(response.body)
+        expect(json.length).to eq(2)
+      end
+    end
+  end
+
+  describe 'GET events/:id' do
+    context 'イベントが存在するとき' do
+      let(:event) { create(:event) }
+
+      before do
+        get "/events/#{event.id}"
+      end
+
+      it '200 OK が返ってくる' do
+        expect(response).to be_successful
+        expect(response.status).to eq 200
+      end
+
+      it 'イベント情報が返ってくる' do
+        json = JSON.parse(response.body)
+        expect(json['id']).to eq(event.id)
+      end
+    end
+  end
+
+  describe 'POST events' do
+    before do
+      post '/events', params: { event: { name: '中級レッスン', date: Time.current } }
+    end
+
+    it '200 OK が返ってくる' do
+      expect(response).to be_successful
+      expect(response.status).to eq 200
+    end
+
+    it 'イベントが登録できている' do
+      json = JSON.parse(response.body)
+      expect(json['name']).to eq('中級レッスン')
+    end
+  end
+
+  describe 'PUT events/:id' do
+    let(:event) { create(:event) }
+
+    before do
+      put "/events/#{event.id}", params: { event: { name: 'イベント名更新' } }
+    end
+
+    it '200 OK が返ってくる' do
+      expect(response).to be_successful
+      expect(response.status).to eq 200
+    end
+
+    it 'イベントが更新できている' do
+      json = JSON.parse(response.body)
+      expect(json['name']).to eq('イベント名更新')
+    end
+  end
+
+  describe 'DELETE events/:id' do
+    let(:event) { create(:event) }
+
+    before do
+      delete "/events/#{event.id}"
+    end
+
+    it '200 OK が返ってくる' do
+      expect(response).to be_successful
+      expect(response.status).to eq 200
+    end
+
+    it 'イベントが削除できている' do
+      expect(Event.count).to eq(0)
+    end
   end
 end
