11b261d70fe542cb5d5f7aa397b5e45a68d11cd9
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-06 08:43:53 +0900

    バリデーション追加

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,2 +1,6 @@
 class Prefecture < ApplicationRecord
+  validates :name, presence: true
+  validates :en_name, presence: true
+  validates :area, presence: true
+  validates :en_area, presence: true
 end
