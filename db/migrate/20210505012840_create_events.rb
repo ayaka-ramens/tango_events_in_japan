22b5935c3406e21fa5cb11b9bf08eadf2b59cd6a
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-05 11:13:30 +0900

    eventsテーブル作成

--- db/migrate/20210505012840_create_events.rb
+++ db/migrate/20210505012840_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.string :name, null: false
+      t.datetime :date, null: false
+      t.integer :prefecture_id
+      t.string :address1
+      t.string :address2
+      t.text :description
 
       t.timestamps
     end
