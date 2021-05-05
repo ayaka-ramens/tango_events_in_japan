809b657df74163a6803d9db218a8a3c8d0c1b5d3
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-06 08:40:28 +0900

    prefectureテーブル作成

--- db/migrate/20210505233630_create_prefectures.rb
+++ db/migrate/20210505233630_create_prefectures.rb
@@ -1,6 +1,10 @@
 class CreatePrefectures < ActiveRecord::Migration[6.0]
   def change
     create_table :prefectures do |t|
+      t.string :name, null: false
+      t.string :en_name, null: false
+      t.string :area, null: false
+      t.string :en_area, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_012840) do
+ActiveRecord::Schema.define(version: 2021_05_05_233630) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_05_05_012840) do
     t.string "address1"
     t.string "address2"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "en_name", null: false
+    t.string "area", null: false
+    t.string "en_area", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
