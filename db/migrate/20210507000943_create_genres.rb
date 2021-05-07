661bf7f97c4a25aec6c460867db865887bb88420
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-07 09:14:02 +0900

    genresテーブル作成

--- db/migrate/20210507000943_create_genres.rb
+++ db/migrate/20210507000943_create_genres.rb
@@ -1,6 +1,8 @@
 class CreateGenres < ActiveRecord::Migration[6.0]
   def change
     create_table :genres do |t|
+      t.string :name, null: false
+      t.boolean :remote, null: false, default: false
 
       t.timestamps
     end
