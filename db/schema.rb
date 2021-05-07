745550e5e9265ddb79c5ae29b97ee6e5a6970a6e
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-07 22:57:48 +0900

    genre_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_000943) do
+ActiveRecord::Schema.define(version: 2021_05_07_135355) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_000943) do
     t.string "address1"
     t.string "address2"
     t.text "description"
+    t.integer "genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
