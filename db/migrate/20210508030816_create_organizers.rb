b3e3acc883c1608f9e8a761e826ae839d35d1d5e
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-08 13:09:40 +0900

    organizersテーブル作成

--- db/migrate/20210508030816_create_organizers.rb
+++ db/migrate/20210508030816_create_organizers.rb
@@ -1,6 +1,14 @@
 class CreateOrganizers < ActiveRecord::Migration[6.0]
   def change
     create_table :organizers do |t|
+      t.string :name, null: false
+      t.string :en_name
+      t.text :description
+      t.integer :prefecture_id
+      t.string :address1
+      t.string :address2
+      t.string :hp_url
+      t.string :mail
 
       t.timestamps
     end
