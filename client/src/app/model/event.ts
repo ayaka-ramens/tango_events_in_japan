0534403f47dbc1021c392a0abe5ad9fc61975e81
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-11 09:26:16 +0900

    モデルクラス作成

---
+++ client/src/app/model/event.ts
@@ -0,0 +1,10 @@
+export class Event {
+  id?: number | null;
+  name?: string | null;
+  date?: string | null;
+  prefecture_id?: number | null;
+  address1?: string | null;
+  address2?: string | null;
+  description?: string | null;
+  genre_id?: number | null;
+}
