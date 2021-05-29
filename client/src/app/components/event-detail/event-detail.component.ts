46fb07e0962c0ae1e1140bc1242c6d077301cab4
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-05-29 13:46:45 +0900

    event-detailコンポーネント作成

--- client/src/app/components/event-detail/event-detail.component.ts
+++ client/src/app/components/event-detail/event-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from 'src/app/services/event/event.service';
+import { Event } from 'src/app/models/event';
 
 @Component({
   selector: 'app-event-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-detail.component.scss']
 })
 export class EventDetailComponent implements OnInit {
+  event: Event | undefined;
 
-  constructor() { }
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getEvent()
+  }
+
+  getEvent():void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.eventService.getEvent(Number(id))
+      .subscribe(event => this.event = event);
   }
 
 }
