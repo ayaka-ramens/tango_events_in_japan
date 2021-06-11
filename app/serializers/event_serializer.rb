ccbf8aeeecf683b9018f3058570137fd0105eec2
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-06-11 09:40:08 +0900

    EventSerializerクラス作成

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,8 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :date, :prefecture, :address1, :address2, :description,
+    :genre_id, :created_at, :updated_at
+
+  def prefecture
+    Prefecture.find(object.prefecture_id).name
+  end
 end
