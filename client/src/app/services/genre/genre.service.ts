4556ea8bf7da80f70e7a0676f8412bba59d04b2a
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-06-28 22:48:34 +0900

    ジャンルサービスをinterfaceにした

--- client/src/app/services/genre/genre.service.ts
+++ client/src/app/services/genre/genre.service.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Genre } from 'src/app/models/genre';
+export interface Genre {
+  id?: number | null;
+  name?: string | null;
+  en_name?: string | null;
+  remote?: boolean | null;
+}
 
-@Injectable({
-  providedIn: 'root'
-})
-export class GenreService {
-  genreData: Genre[] = [
+export const genres: Genre[] = [
     { id: 1, name: "ミロンガ", en_name: "milonga", remote: false},
     { id: 2, name: "レッスン", en_name: "lesson", remote: false},
     { id: 3, name: "プラクティカ", en_name: "practice", remote: false},
     { id: 4, name: "勉強会", en_name: "study", remote: false},
     { id: 5, name: "その他ローカル", en_name: "other local", remote: false},
-    { id: 6, name: "ミロンガ", en_name: "milonga", remote: true},
-    { id: 7, name: "レッスン", en_name: "lesson", remote: true},
-    { id: 8, name: "勉強会", en_name: "study", remote: true},
-    { id: 9, name: "その他リモート", en_name: "other remote", remote: true}
-  ]
-
-  constructor() { }
-}
+    { id: 6, name: "オンラインミロンガ", en_name: "milonga", remote: true},
+    { id: 7, name: "オンラインレッスン", en_name: "lesson", remote: true},
+    { id: 8, name: "オンライン勉強会", en_name: "study", remote: true},
+    { id: 9, name: "オンラインその他", en_name: "other remote", remote: true}
+]
