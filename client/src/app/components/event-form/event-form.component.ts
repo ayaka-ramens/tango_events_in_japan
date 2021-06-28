d215d927e2f589300e2c0752c1d60ef1dd966e37
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-06-28 22:47:10 +0900

    フォームコンポーネント作成
    コンソールに値表示できるまで

--- client/src/app/components/event-form/event-form.component.ts
+++ client/src/app/components/event-form/event-form.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { EventService } from 'src/app/services/event/event.service';
+import { Event } from '../../models/event';
+import { Genre, genres } from 'src/app/services/genre/genre';
+import { Prefecture, prefectures } from 'src/app/services/prefecture/prefecture';
 
 @Component({
-  selector: 'app-event-form',
+  selector: 'event-form',
   templateUrl: './event-form.component.html',
   styleUrls: ['./event-form.component.scss']
 })
 export class EventFormComponent implements OnInit {
+  prefectureData: Prefecture[] = [];
+  genreData: Genre[] = [];
 
-  constructor() { }
+  // TODO バリデーションちゃんと書く＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+  eventForm = this.fb.group({
+    name: ['', Validators.required],
+    date: [''],
+    prefecture_id: [''],
+    address1: [''],
+    address2: [''],
+    description: [''],
+    genre_id: ['']
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private eventService: EventService
+  ) { }
 
   ngOnInit(): void {
+    this.getPrefectures();
+    this.getGenre();
   }
 
+  getPrefectures(): void {
+    this.prefectureData = prefectures
+  }
+
+  getGenre(): void {
+    this.genreData = genres
+  }
+
+  createEvent(): void {
+    console.log(this.eventForm.value)
+    // this.eventService.createEvent(event)
+    //   .subscribe(event => {
+    //     // TODO 通知する
+    //     console.log(`created: ${event.name}`);
+    //   })
+  }
 }
