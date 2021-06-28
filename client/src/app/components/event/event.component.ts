9d01aedd4a79bb1e2dae06e3e3be5483562f2f3e
Ayaka Moronaga <a.moronaga@everyleaf.com>
2021-06-28 22:46:17 +0900

    イベントコンポーネントから都道府県とジャンルの処理を削除
    フォームコンポーネントで行うため

--- client/src/app/components/event/event.component.ts
+++ client/src/app/components/event/event.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../services/event/event.service';
 import { Event } from '../../models/event';
-import { Genre, genres } from 'src/app/services/genre/genre';
-import { Prefecture, prefectures } from 'src/app/services/prefecture/prefecture';
 
 
 @Component({
@@ -11,31 +9,16 @@ import { Prefecture, prefectures } from 'src/app/services/prefecture/prefecture'
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
-  prefectureData: Prefecture[] = [];
-  genreData: Genre[] = [];
   eventList: Event[] = [];
 
-  constructor(
-    private eventService: EventService,
-    ) { }
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
-    this.getPrefectures();
-    this.getGenre();
     this.getEvents();
-  }
-
-  getPrefectures():void {
-    this.prefectureData = prefectures
-  }
-
-  getGenre(): void {
-    this.genreData = genres
   }
 
   getEvents(): void {
     this.eventService.getEvents()
       .subscribe(eventList => this.eventList = eventList)
   }
-
 }
